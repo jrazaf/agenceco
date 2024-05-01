4da65424d3486afe9becfb19608071f13f9e0c4c
jose remi <jose.azavana@gmail.com>
2024-05-01 09:11:42 +0200

    initialisation code agence web eco

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const links = document.querySelectorAll("nav li");
+
+icons.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+          // Défilement fluide jusqu'à la section cible
+          targetElement.scrollIntoView({ block: 'start' });
+      }
+  });
+});
